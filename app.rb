c91b3cebb945c367a1676f62ce36b74b335d8624
Evan_Ratzlaff <ratzlaff92@gmail.com>
2019-08-01 00:26:24 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,14 +6,41 @@ class App < Sinatra::Base
     "#{@user_name}"
   end
   get '/square/:number' do 
-     @sq_num = params[:num].to_i**2
+     @sq_num = (params[:number].to_i**2).to_s
      "#{@sq_num}"
   end 
   get '/say/:number/:phrase' do
+    answer = ''
+    params[:number].to_i.times do 
+      answer += params[:phrase]
+    end 
+    answer
   end
   get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end 
   get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i 
+    number2 = params[:number2].to_i
+    if params[:operation] = 'add'
+      answer = (number1 + number2).to_s
+    elsif params[:operation] = 'subtract'
+      answer2 = (number2 - number1).to_s
+    elsif params[:operation] = 'multiply'
+      answer3 = (number1 * number2).to_s
+    elsif params[:operation] = 'divide'
+      answer4 = (number1 / number2).to_s
+    else 
+      nil
+    end
+    answer
+    answer2
+    answer3
+    answer4
+    end 
+    
+    
+    
   end
 
 
